3ea7021b01dadf89a1cd8877459c45e90f81beba
faisalabdulghani <faisalabdulghani12@gmail.com>
2024-08-21 14:03:56 +0500

    NavBar add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import NavBar from './components/NavBar';
 import TermsAndCondition from './page/termsconditions/TermsAndCondition';
 import PrivacyPolicy from './page/PrivacyPolicy/PrivacyPolicy';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <TermsAndCondition />
-      <PrivacyPolicy />
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        {/* Set TermsAndCondition as the default home page */}
+        <Route path="/" element={<TermsAndCondition />} />
+        <Route path="/termsandcondition" element={<TermsAndCondition />} />
+        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+        {/* Redirect any unknown paths to the home page */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <nav style={{ backgroundColor: '#333', padding: '1rem' }}>
+            <ul style={{ display: 'flex', listStyle: 'none', margin: 0, padding: 0 }}>
+                <li style={{ marginRight: '1rem' }}>
+                    <Link to="/termsandcondition" style={{ color: '#fff', textDecoration: 'none' }}>
+                        Terms and Conditions
+                    </Link>
+                </li>
+                <li>
+                    <Link to="/privacypolicy" style={{ color: '#fff', textDecoration: 'none' }}>
+                        Privacy Policy
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default NavBar;
